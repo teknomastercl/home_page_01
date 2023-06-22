2154ac865a2adec104965c96fc96b3ce8733c465
Misael Trincado <misatrincado@gmail.com>
2023-06-22 13:54:18 -0400

    upload proyect

--- src/modules/home/Footer/RedesSociales.tsx
+++ src/modules/home/Footer/RedesSociales.tsx
@@ -10,10 +10,10 @@ interface Props {
 const RedesSociales:React.FC<Props> = ({className}) => {
     return (
         <div className={className}>
-            <a href="https://www.facebook.com/teknomaster.cl" target="_blank">
+            <a href="https://www.facebook.com/profile.php?id=100091750896599" target="_blank">
                 <FacebookIcon className={iconStyle} />
             </a>
-            <a href="https://www.instagram.com/teknomaster.cl/" target="_blank">
+            <a href="https://www.instagram.com/teknomaster.ti/" target="_blank">
                 <InstagramIcon className={iconStyle} />
             </a>
             <a href="https://www.linkedin.com/in/tekno-master-ab7560202/" target="_blank">
